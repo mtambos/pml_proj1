96ace788a42918e3d6ae0bd74aa372e500e0e48f
Mario Tambos <mtambos@gmail.com>
2017-12-15 17:28:08 +0100

    improve plotting

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,3 @@
-import itertools
-
 from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
@@ -12,9 +10,20 @@ from sklearn.metrics import (accuracy_score, auc, roc_curve, confusion_matrix,
 from sklearn.model_selection import cross_validate, RepeatedStratifiedKFold
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from yellowbrick.classifier import DecisionBoundariesVisualizer
 
-heatmap_cmap = ListedColormap(sns.color_palette('rocket').as_hex())
+HEATMAP_CMAP = ListedColormap(
+    sns.color_palette('rocket', n_colors=100).as_hex()
+)
+DATA_COLORS = (
+    (0.29803921568627451, 0.44705882352941179, 0.69019607843137254),
+    (0.33333333333333331, 0.6588235294117647, 0.40784313725490196),
+    (0.7686274509803922, 0.30588235294117649, 0.32156862745098042),
+    (0.80000000000000004, 0.72549019607843135, 0.45490196078431372),
+    (0.39215686274509803, 0.70980392156862748, 0.80392156862745101),
+    (0.50588235294117645, 0.44705882352941179, 0.69803921568627447),
+)
+DATA_CMAP = ListedColormap(DATA_COLORS)
+DATA_MARKERS = ('^', 'v', 's', 'd')
 
 
 def matlab2np(mlab_array):
@@ -69,32 +78,35 @@ def plot_decision_boundaries(model, X_train, y_train,
                              X_test, y_test, ax=None):
     if ax is None:
         ax = plt.figure(figsize=(8, 8)).gca()
-    dv = DecisionBoundariesVisualizer(model, classes=[0, 1, 2, 3])
-    markers = (next(dv.markers), next(dv.markers),
-               next(dv.markers), next(dv.markers))
-    colors = dv.colors
-    dv = DecisionBoundariesVisualizer(model, classes=[-1, 1],
-                                      ax=ax, step_size=0.025,
-                                      show_scatter=False,
-                                      pcolormesh_alpha=0.25)
 
-    ax.scatter(*X_train[y_train == -1].T, color=colors[0],
-               edgecolors='black', marker=markers[0], s=30,
+    X = np.r_[X_train, X_test]
+    x_min, x_max = X[:, 0].min() * 1.1, X[:, 0].max() * 1.1
+    y_min, y_max = X[:, 1].min() * 1.1, X[:, 1].max() * 1.1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.1),
+                         np.arange(y_min, y_max, 0.1))
+
+    Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    ax.contourf(xx, yy, Z, alpha=0.4, cmap=DATA_CMAP)
+
+    ax.scatter(*X_train[y_train == -1].T, color=DATA_COLORS[0],
+               edgecolors='black', marker=DATA_MARKERS[0], s=50,
                label='-train')
-    ax.scatter(*X_train[y_train == 1].T, color=colors[1],
-               edgecolors='black', marker=markers[1], s=30,
+    ax.scatter(*X_train[y_train == 1].T, color=DATA_COLORS[1],
+               edgecolors='black', marker=DATA_MARKERS[1], s=50,
                label='+train')
-    ax.scatter(*X_test[y_test == -1].T, color=colors[2],
-               edgecolors='black', marker=markers[2], s=30,
+    ax.scatter(*X_test[y_test == -1].T, color=DATA_COLORS[0],
+               edgecolors='black', marker=DATA_MARKERS[2], s=50,
                label='-test')
-    ax.scatter(*X_test[y_test == 1].T, color=colors[3],
-               edgecolors='black', marker=markers[3], s=30,
+    ax.scatter(*X_test[y_test == 1].T, color=DATA_COLORS[1],
+               edgecolors='black', marker=DATA_MARKERS[3], s=50,
                label='+test')
-    dv.fit_draw_poof(X_test, y_test)
+    ax.grid()
     ax.legend()
 
 
-def plot_kernel_importances(e_mu_orig, kernel_attrs, colormap=heatmap_cmap):
+def plot_kernel_importances(e_mu_orig, kernel_attrs, colormap=HEATMAP_CMAP):
     kernel_attrs = np.asarray(kernel_attrs)
     df = pd.DataFrame(np.c_[e_mu_orig, kernel_attrs],
                       columns=['e_mu', 'kernel', 'features', 'parameter'])\
@@ -109,78 +121,31 @@ def plot_kernel_importances(e_mu_orig, kernel_attrs, colormap=heatmap_cmap):
         group = group.pivot("parameter", "features", "e_mu")
         sns.heatmap(group, vmin=vmin, vmax=vmax, cmap=colormap, ax=ax)
         ax.set_title(kernel)
+    plt.suptitle(r'Kernel importance plot ($e_\mu$)')
 
 
-def evaluate_model(model, X, y, kernel_attrs, cmap=heatmap_cmap):
-    X_train, X_test, y_train, y_test =\
-        train_test_split(X, y, test_size=0.2, stratify=y)
+def plot_e_a_mu(model1, model2, model1_name, model2_name):
+    fig, axes = plt.subplots(1, 2, figsize=(16, 8))
+    ax1, ax2 = axes
+    model1.plot_e(ax=ax1, rug=True, rug_kws={'color': 'r', 'linewidth': 2,
+                                             'alpha': 1})
+    model2.plot_e(ax=ax2, rug=True, rug_kws={'color': 'r', 'linewidth': 2,
+                                             'alpha': 1})
+    ax1.set_title(model1_name)
+    ax2.set_title(model2_name)
+    fig.suptitle(r'$e_\mu$ distplot')
+    plt.tight_layout()
 
-    model.fit(X_train, y_train)
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.gca()
-    model.plot_bounds(ax=ax)
-
-    plot_kernel_importances(model.e_mu_orig, kernel_attrs, cmap)
-
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.gca()
-    model.plot_e(rug=True, ax=ax,
-                 rug_kws={'color': 'r', 'linewidth': 2,
-                          'alpha': 1})
-    ax.set_title(r'$e_\mu$ distplot')
-
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.gca()
-    model.plot_a(rug=True, ax=ax,
-                 rug_kws={'color': 'r', 'linewidth': 2,
-                          'alpha': 1})
-    ax.set_title(r'$a_\mu$ distplot')
-
-    y_pred = model.predict(X_test)
-
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.gca()
-    plot_classification_report(y_test, y_pred, ax=ax)
-
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.gca()
-    plot_confusion_matrix(y_test, y_pred, cmap=cmap, ax=ax)
-
-    y_score = model.predict_proba(X_test)[:, 1]
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.gca()
-    plot_rocauc(y_test, y_score, ax=ax)
-
-    fig = plt.figure(figsize=(8, 8))
-    ax = fig.gca()
-    plot_decision_boundaries(model, X_train, y_train, X_test, y_test, ax=ax)
-
-    model_cv = clone(model)
-    scoring.iteration = 0
-    folds = RepeatedStratifiedKFold(n_splits=3, n_repeats=2)
-    return cross_validate(model_cv, X, y, cv=folds, scoring=scoring)
-
-
-def plot_classification_report(y_test, y_pred, ax=None):
-    if ax is None:
-        ax = plt.figure(figsize=(8, 8)).gca()
-
-    xticks = ['precision', 'recall', 'f1-score', 'support']
-    yticks = list(np.unique(y_test))
-    yticks += ['avg']
-
-    rep = np.array(precision_recall_fscore_support(y_test, y_pred)).T
-    avg = np.mean(rep, axis=0)
-    avg[-1] = np.sum(rep[:, -1])
-    rep = np.insert(rep, rep.shape[0], avg, axis=0)
-
-    sns.heatmap(rep,
-                annot=True,
-                cbar=False,
-                xticklabels=xticks,
-                yticklabels=yticks,
-                ax=ax, cmap=heatmap_cmap)
-    return
+    fig, axes = plt.subplots(1, 2, figsize=(16, 8))
+    ax1, ax2 = axes
+    model1.plot_a(ax=ax1, rug=True, rug_kws={'color': 'r', 'linewidth': 2,
+                                             'alpha': 1})
+    model2.plot_a(ax=ax2, rug=True, rug_kws={'color': 'r', 'linewidth': 2,
+                                             'alpha': 1})
+    ax1.set_title(model1_name)
+    ax2.set_title(model2_name)
+    fig.suptitle(r'$a_\mu$ distplot')
+    plt.tight_layout()
 
 
 def plot_rocauc(y_test, y_score, ax=None):
@@ -202,43 +167,60 @@ def plot_rocauc(y_test, y_score, ax=None):
     return
 
 
-def plot_confusion_matrix(y_test, y_pred, cmap=heatmap_cmap, ax=None):
-    """
-    This function prints and plots the confusion matrix.
-    """
-    cm = confusion_matrix(y_test, y_pred)
+def plot_classification_report(y_test, y_pred, ax=None, cbar=True,
+                               cbar_ax=None, cbar_orient='vertical'):
+    assert cbar_orient in ('horizontal', 'vertical')
+    if ax is None:
+        ax = plt.figure(figsize=(8, 8)).gca()
 
-    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    xticks = ['precision', 'recall', 'f1-score', 'support']
+    yticks = list(np.unique(y_test))
+    yticks += ['avg']
+
+    rep = np.array(precision_recall_fscore_support(y_test, y_pred)).T
+    avg = np.mean(rep, axis=0)
+    avg[-1] = np.sum(rep[:, -1])
+    rep = np.insert(rep, rep.shape[0], avg, axis=0)
+
+    sns.heatmap(rep[:, :-1],
+                annot=True,
+                xticklabels=xticks,
+                yticklabels=yticks,
+                ax=ax,
+                vmin=0,
+                vmax=1,
+                cbar=cbar,
+                cbar_ax=cbar_ax,
+                cbar_kws={"orientation": cbar_orient})
+
+    support = np.zeros_like(rep) * np.nan
+    support[:, -1] = rep[:, -1]
+    sns.heatmap(support,
+                annot=True,
+                cbar=False,
+                xticklabels=xticks,
+                yticklabels=yticks,
+                ax=ax)
+
+    return
+
+
+def plot_confusion_matrix(y_test, y_pred, cmap=HEATMAP_CMAP, ax=None,
+                          cbar=True, cbar_ax=None, cbar_orient='vertical'):
+    assert cbar_orient in ('horizontal', 'vertical')
+    cm = confusion_matrix(y_test, y_pred)
+    cm = cm / cm.sum(axis=1, keepdims=True)
 
     classes = [-1, 1]
     if ax is None:
         ax = plt.figure(figsize=(8, 8)).gca()
 
-    ax_pos = ax.get_position()
-    cbar_pos = [
-        ax_pos.x0 + ax_pos.width*0.05,
-        ax_pos.y0 - 0.043,  # + ax_pos.height,
-        ax_pos.width - ax_pos.width*0.1,
-        ax_pos.height*0.04,
-    ]
-    fig = ax.figure
-    cax = fig.add_axes(cbar_pos)
-    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
-    fig.colorbar(im, cax=cax, orientation='horizontal')
-    cax.tick_params(labelsize='xx-small', )
-
-    tick_marks = np.arange(len(classes))
-    ax.set_xticks(tick_marks)
-    ax.set_xticklabels(classes, rotation=45)
-    ax.set_yticks(tick_marks)
-    ax.set_yticklabels(classes)
-
-    fmt = '.2f'
-    thresh = cm.max() - (cm.max() - cm.min()) * 0.2
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        ax.text(j, i, format(cm[i, j], fmt),
-                horizontalalignment="center",
-                color="white" if cm[i, j] < thresh else "black")
+    sns.heatmap(cm, ax=ax, annot=True,
+                xticklabels=classes, yticklabels=classes,
+                cbar=cbar,
+                cbar_ax=cbar_ax,
+                cbar_kws={"orientation": cbar_orient},
+                vmin=0, vmax=1)
 
     ax.set_ylabel('True label')
     ax.yaxis.set_label_coords(-0.03, 0.5)
@@ -247,10 +229,69 @@ def plot_confusion_matrix(y_test, y_pred, cmap=heatmap_cmap, ax=None):
     return
 
 
-def plot_metrics(y_true, y_pred, y_scores, axes=None):
+def plot_metrics(y_true, y_pred, y_scores, axes=None, cbar_orient='vertical'):
     if axes is None:
         _, axes = plt.subplots(3, 1)
     ax1, ax2, ax3 = axes.flatten()
-    plot_classification_report(y_true, y_pred, ax=ax1)
-    plot_confusion_matrix(y_true, y_pred, ax=ax2)
+    plot_classification_report(y_true, y_pred, ax=ax1, cbar_orient=cbar_orient)
+    plot_confusion_matrix(y_true, y_pred, ax=ax2, cbar_orient=cbar_orient)
     plot_rocauc(y_true, y_scores, ax=ax3)
+
+
+def evaluate_model(model, X, y, kernel_attrs, cmap=HEATMAP_CMAP):
+    X_train, X_test, y_train, y_test =\
+        train_test_split(X, y, test_size=0.2, stratify=y)
+
+    model.fit(X_train, y_train)
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.gca()
+    model.plot_bounds(ax=ax)
+    sns.despine()
+    plt.tight_layout()
+
+    plot_kernel_importances(model.e_mu_orig, kernel_attrs, cmap)
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.gca()
+    model.plot_e(rug=True, ax=ax,
+                 rug_kws={'color': 'r', 'linewidth': 2,
+                          'alpha': 1})
+    ax.set_title(r'$e_\mu$ distplot')
+    plt.tight_layout()
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.gca()
+    model.plot_a(rug=True, ax=ax,
+                 rug_kws={'color': 'r', 'linewidth': 2,
+                          'alpha': 1})
+    ax.set_title(r'$a_\mu$ distplot')
+    plt.tight_layout()
+
+    y_pred = model.predict(X_test)
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.gca()
+    plot_classification_report(y_test, y_pred, ax=ax)
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.gca()
+    plot_confusion_matrix(y_test, y_pred, cmap=cmap, ax=ax,
+                          cbar_orient='vertical')
+
+    y_score = model.predict_proba(X_test)[:, 1]
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.gca()
+    plot_rocauc(y_test, y_score, ax=ax)
+    sns.despine()
+    plt.tight_layout()
+
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.gca()
+    plot_decision_boundaries(model, X_train, y_train, X_test, y_test, ax=ax)
+    sns.despine()
+    plt.tight_layout()
+
+    model_cv = clone(model)
+    scoring.iteration = 0
+    folds = RepeatedStratifiedKFold(n_splits=3, n_repeats=2)
+    return cross_validate(model_cv, X, y, cv=folds, scoring=scoring)
